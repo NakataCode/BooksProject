ce1b2611c88d739005257f7f07dd2f2fb418ae41
Niki A <nikolaanachkov03@gmail.com>
2022-12-02 22:48:42 +0200

    commiting project

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="containerOne">
+        <img src="/images/sapiens.png"></img>
+        <img src="/images/designofeveryday.png"></img>
+        <img src="/images/innovator.png"></img>
+      </div>
+      <div className="containerTwo">
+        <img src="/images/joking.png"></img>
+        <img src="/images/mindset.png"></img>
+        <img src="/images/powerofhabit.png"></img>
+      </div>
+      <div className="containerThree">
+        <img src="/images/toolsoftitans.png"></img>
+      </div>
+    </div>
+  );
+}
+
+export default App;
